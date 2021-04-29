ee3eb54b4fa05042d82da08af7989faaf34408bd
Martha Ramirez <iramirez@asdeporte.com>
2021-04-29 14:50:00 -0500

    init

---
+++ producer.py
@@ -0,0 +1,12 @@
+import pika, json
+from decouple import config
+
+params = pika.URLParameters(config('URL_PARAMETER'))
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
